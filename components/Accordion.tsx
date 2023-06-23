65b830d76a90fd9fc30eb4e7fec5262adec585a8
Denis <like-nix@mail.ru>
2023-06-24 00:33:13 +0300

    add accordion

---
+++ components/Accordion.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React from "react";
+import styles from "./accordion.module.scss";
+
+type EventHandlerOpenClose = (event: React.MouseEvent<HTMLDivElement>) => void;
+
+type EventHandlerTransitionEnd = (
+  event: React.TransitionEvent<HTMLDivElement>
+) => void;
+
+const Accordion = ({ title, text }: { title: string; text: string }) => {
+  const refTextNode = React.useRef<HTMLDivElement>(null);
+  const [classShow, setClassShow] = React.useState(false);
+  const show = React.useRef(false);
+  const [heightDropDown, setHeightDropDown] = React.useState<{
+    height?: string;
+  }>({ height: "0px" });
+
+  const handleOpenClose: EventHandlerOpenClose = (e) => {
+    e.stopPropagation();
+    if (refTextNode.current === null) return;
+    const heightContent =
+      refTextNode.current.getBoundingClientRect().height + "px";
+    if (!show.current) {
+      show.current = true;
+      setHeightDropDown({height: heightContent});
+    } else {
+      show.current = false;
+      if (classShow) {
+        setHeightDropDown({height: heightContent});
+        setTimeout(() => {
+          setClassShow(false);
+          setHeightDropDown({});
+        }, 0);
+      }
+      else{
+        setHeightDropDown({});
+      }
+    }
+  };
+
+  const handleTransitionEnd: EventHandlerTransitionEnd = (e) => {
+    e.stopPropagation();
+    if (show.current) {
+      setClassShow(true);
+      setHeightDropDown({});
+    }
+  };
+
+  return (
+    <div className={styles.accordion}>
+      <div className={styles.title} onClick={handleOpenClose}>
+        {title}
+      </div>
+      <div
+        className={`${styles.dropDown} ${classShow ? styles.show : ""}`}
+        onTransitionEnd={handleTransitionEnd}
+        style={heightDropDown}
+      >
+        <div className={styles.text} ref={refTextNode}>
+          {text}
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Accordion;
