63f893a96eda2bd9a4512a372479b2bdd252f4f3
Denis <like-nix@mail.ru>
2023-06-26 06:32:30 +0300

    Перееработка фильтра

---
+++ components/Filter2/Field.tsx
@@ -0,0 +1,18 @@
+import styles from "./filter.module.scss";
+
+const Field = ({
+  children,
+  name,
+}: {
+  children: React.ReactNode;
+  name: string;
+}) => {
+  return (
+    <div className={styles.field}>
+      {/* <div className={styles.name}>{name}</div>
+      <div className={styles.component}>{children}</div> */}
+    </div>
+  );
+};
+
+export default Field;
---
+++ components/Filter2/index.tsx
@@ -0,0 +1,23 @@
+// "use client";
+import styles from "./filter.module.scss";
+import DropDown from "./DropDown";
+import Field from "./Field";
+
+
+const Filter = () => {
+  return (
+    <div className={styles.block}>
+      <div className={styles.title}>Фильтр поиска</div>
+      {/* <Field name="Название фильма">
+        <Input placeholder="Введите название" />
+      </Field>
+      <Field name="Жанр">
+        <Select placeholder="Введите жанр" />
+      </Field> */}
+      <Field name="Кинотеатр">
+        <DropDown placeholder="Введите кинотеатр" />
+      </Field>
+    </div>
+  );
+};
+export default Filter;
